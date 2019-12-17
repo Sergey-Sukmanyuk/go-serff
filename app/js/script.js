9bf5c05565df8e5b245fc03acb73344bd8459d9c
Sergey-Sukmanyuk <ssukmanyuk@gmail.com>
2019-12-17 07:02:32 +0200

    go-serff - new project, work with figma, gulp, scss

---
+++ app/js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="Arrow">',
+        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="Arrow">',
+        asNavFor: '.slider__dots'
+    });
+
+    $('.slider__dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+
+    })
+
+});
