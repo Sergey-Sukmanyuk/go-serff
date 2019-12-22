a7087e4e39c49e2c719f4007021ff0225693b93b
Sergey-Sukmanyuk <ssukmanyuk@gmail.com>
2019-12-22 18:06:45 +0200

    made three block

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
     $('.header__slider').slick({
         infinite: true,
@@ -14,11 +14,28 @@ $(function(){
         asNavFor: '.header__slider'
     })
 
-    $('.serf__slider').slick({
+    $('.surf__slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="Arrow">',
         nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="Arrow">',
+        asNavFor: '.slider-map'
+
     })
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf__slider',
+        focusOnSelect: true
+    });
+
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="Arrow">',
+        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="Arrow">',
+    });
 
 });
